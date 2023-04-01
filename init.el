c8f60b035a30a0e2cfeb4eeedfb397c6b5c3bbe5
yuzu-eva <stevenhu@web.de>
2023-04-01 18:50:14 +0200

    init commit

---
+++ init.el
@@ -0,0 +1,44 @@
+;; set load-path
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+;; Make emacs startup faster
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+ 
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+ 
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+ 
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+    gc-cons-percentage 0.1))
+ 
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+;;
+
+;; Initialize melpa repo
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+        '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Initialize use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Initialize rainbow-delimiters
+(require 'rainbow-delimiters)
+
+(set-default-file-modes #o700)
+
+(load-theme 'dracula-custom t)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
